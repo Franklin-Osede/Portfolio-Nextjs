a2873e76db2e1abf0101a6012eeb0e8413234361
Franklin <71532004+fran1014@users.noreply.github.com>
2025-01-29 19:05:49 +0100

    added motion effect to know me section

--- src/components/KnowMeSection.tsx
+++ src/components/KnowMeSection.tsx
@@ -1,10 +1,17 @@
 'use client';
 
+import { motion } from 'framer-motion'; 
 import dynamic from 'next/dynamic';
 
 const KnowMeSection = () => {
   return (
-    <section id="know-me" className=" bg-darkBlue relative overflow-hidden">
+    <motion.section
+    id="know-me"
+    className="bg-darkBlue relative overflow-hidden"
+    initial={{ opacity: 0, y: 50 }} // 🔹 Sección aparece con fade-in y se desliza hacia arriba
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.8, ease: "easeOut" }}
+  >
       <div className="absolute inset-0 bg-gradient-to-b from-darkBlue to-navyBlue opacity-50" />
       <div className="relative z-10 container mx-auto px-4 py-8">
         <div className="max-w-6xl mx-auto">
@@ -22,7 +29,7 @@ const KnowMeSection = () => {
           </div>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
